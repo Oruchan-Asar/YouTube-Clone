98314cc2d2719afcde9d829dbd711529b21d0cde
Oruchan Asar <oruchan.asar@gmail.com>
2022-08-27 13:00:22 +0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
