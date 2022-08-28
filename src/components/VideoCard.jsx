44ca7194ec865787c0469ef7daf5f4c0ebf333ef
Oruchan Asar <oruchan.asar@gmail.com>
2022-08-28 15:55:05 +0300

    modify CardMedia image

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -26,7 +26,7 @@ const VideoCard = ({
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.high?.url}
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
           sx={{ width: { xs: "100%", sm: "358px", md: "320px" }, height: 180 }}
         />
